631d9a7e3a771d3f942b22dfe6fe431e85e2c372
iTecX <30746447+iTecAI@users.noreply.github.com>
2019-12-10 08:48:15 -0500

    more work on crypto

--- __init__.py
+++ __init__.py
@@ -7,29 +7,29 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import time
 import secrets
+from requests import get
 
 def ip():
     return socket.gethostbyname(socket.gethostname())
 
-def generate_salt():
-    t = time.time()
-    token = secrets.SystemRandom(t)
-
+def p_ip():
+    return get('https://api.ipify.org').text
 
 class Crypt:
     def __init__(self,password,peer):
         self.peer = peer
+        self.key = self.gen_key(password)
 
-    def gen_key(self,password,salt):
+    def gen_key(self,password):
         password = bytes(password,'utf-8')
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
-            salt=salt,
+            salt=bytes(p_ip(),'utf-8'),
             iterations=100000,
             backend=default_backend()
         )
-        key = base64.urlsafe_b64encode(kdf.derive(password))
+        return base64.urlsafe_b64encode(kdf.derive(password))
 
 class NodeServer:
     def __init__(self,peer,addr):
@@ -40,11 +40,12 @@ class NodeServer:
         conn.sendall()
 
 class Peer:
-    def __init__(self,localport,identifier,method_class,connection_methods=[]):
+    def __init__(self,localport,identifier,network_password,method_class,connection_methods=[]):
         self.cmethods = connection_methods
         self.mclass = method_class
         self.id = identifier
         self.port = localport
         self.node_servers = []
+        self.crypt = Crypt(network_password,self)
     def bootstrap(self,addr):
         pass
